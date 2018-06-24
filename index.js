edbd7b8a91206359f59d3927eb48f0b9e5f2a350
Emily Cubbage <emilycubbage@gmail.com>
2018-06-24 17:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function postComment() {
 function createPost() {
   var author = document.getElementById("authorName").value;
   var title = document.getElementById("postTitle").value;
-  var commentTemplate = document.getElementById("comment-template").innerHTML;
+  var body = document.getElementById("postBody").value;
+  var postTemplate = document.getElementById("post-template").innerHTML;
  
   //create template function
   var templateFn = _.template(commentTemplate);
